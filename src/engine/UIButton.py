95b3eeb5845065b3a6719b7090b718e74fdb7502
brtrahms <42851844+brtrahms@users.noreply.github.com>
2021-11-15 13:17:05 -0800

    UIButton class

--- src/engine/UIButton.py
+++ src/engine/UIButton.py
@@ -3,4 +3,21 @@ from pygame.sprite import Sprite
 
 
 class UIButton(Sprite):
-	pass
+
+	def __init__(self, text, width, height):
+		self.__text = text
+		self.__highlighted = False
+		self.__active = True
+		self.__bounding_box
+
+	def set_active_status(self, status):
+		self.__active = status
+
+	def get_active_status(self):
+		return self.__active
+
+	def set_highlight_status(self, status):
+		self.__highlighted = status
+
+	def get_highlight_status(self):
+		return self.__highlighted
