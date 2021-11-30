0c913d4dc666a93e924ab22c3bcfe4e47e0eabb4
brtrahms <42851844+brtrahms@users.noreply.github.com>
2021-11-30 15:34:13 -0800

    Grid_Square

--- src/gameplay/Grid_Square.py
+++ src/gameplay/Grid_Square.py
@@ -1,12 +1,18 @@
-
+import Dot
 
 class Grid_Square:
     
     def __init__(self):
-        __textureID =
+        __textureID = 0
 
 class Path(Grid_Square):
-    pass
+    
+    def __init__(self):
+        super().__init__()
+        __dot_instance = Dot()
 
 class Blockade(Grid_Square):
-    pass
+    
+    def __init__(self):
+        super().__init__()
+        pass
