27577170abba5dab7723ab44fce8202eabce772b
Brady Beskeen <70493398+bjbeskeen@users.noreply.github.com>
2021-12-01 16:51:40 -0800

    Update UIButton.py

--- src/engine/UIButton.py
+++ src/engine/UIButton.py
@@ -1,15 +1,22 @@
 from pygame.sprite import Sprite
 import pygame as pyg
+from pygame import Color
+from pygame.font import Font, SysFont
 
 
 
 class UIButton(Sprite):
 
-	def __init__(self, text, width, height):
+	def __init__(self, text, width, height, text_font = SysFont('Calibri', 18, bold = False, italic = False), text_color = Color(255, 255, 255), background_color = Color(  0,   0,   0), border_color = Color(255, 255, 255)):
 		self.__text = text
 		self.__highlighted = False
 		self.__active = True
 		self.__bounding_box = pyg.Rect(0, 0, width, height)
+		
+		self.text_font             = text_font
+		self.text_color            = text_color
+		self.background_color      = background_color
+		self.border_color          = border_color
 
 	def set_active_status(self, status):
 		self.__active = status
